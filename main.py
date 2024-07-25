7e52e164a894bb0eeca2beb363427dc7668a24e7
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2024-07-25 19:56:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[0].id)
 rate = engine.setProperty("rate",170)
 
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
 
 
 def takeCommand():
