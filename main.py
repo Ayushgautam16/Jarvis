daa8e50aaebd205f7b0f2fce16cbe60dbcbf0131
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2024-07-27 22:28:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import pyttsx3
-import speech_recognition 
-engine = pyttsx3.init("sapi5")
-voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[0].id)
-rate = engine.setProperty("rate",170)
+
 
 def speak(audio):
     engine.say(audio)
